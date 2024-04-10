b057cea178ae34bfc48a137a363f35c80b6bbbf9
Shreejal Maharjan <shreejal27@gmail.com>
2024-04-10 10:23:18 +0545

    added sender code to send messages

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+        {
+        var senderFactory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "admin",
+            Password = "admin",
+        };
+        using (var senderConnection = senderFactory.CreateConnection())
+        using (var senderChannel = senderConnection.CreateModel())
+        {
+            var message = "Hello, RabbitMQ!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            senderChannel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+            senderChannel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+    }
+}
